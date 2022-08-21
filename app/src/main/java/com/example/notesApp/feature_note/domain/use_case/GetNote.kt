d1635d97b23b4febeb5f4e7f3046a23a7e39b443
Krishna Satish <i.am.krishnasatish@gmail.com>
2022-08-21 22:27:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notesApp/feature_note/domain/use_case/GetNote.kt
@@ -0,0 +1,13 @@
+package com.example.notesApp.feature_note.domain.use_case
+
+import com.example.notesApp.feature_note.domain.model.Note
+import com.example.notesApp.feature_note.domain.repository.NoteRepository
+
+class GetNote(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id)
+    }
+}
